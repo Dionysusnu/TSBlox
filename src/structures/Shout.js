05c1286d3d58cf4e3bd5210ee144d27dd91f4438
Dionysusnu <dion.niessen@gmail.com>
2019-09-08 20:08:49 +0200

    fix bugs

--- src/structures/Shout.js
+++ src/structures/Shout.js
@@ -4,9 +4,9 @@ const User = require('./User');
 module.exports = class Shout extends Base {
 	constructor(client, data) {
 		super(client);
-		Object.defineProperty(this, 'message', data.body);
-		Object.defineProperty(this, 'poster', client.users.get(data.poster.userId) || new User(data.poster));
-		Object.defineProperty(this, 'timeUpdated', data.updated);
+		Object.defineProperty(this, 'message', { value: data.body });
+		Object.defineProperty(this, 'poster', { value: client.users.get(data.poster.userId) || new User(client, data.poster) });
+		Object.defineProperty(this, 'timeUpdated', { value: data.updated });
 		// time subject to change
 	}
 };
