ce0a0b2aaba1a07b6387d6609bbab4c9e2de8ceb
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2019-09-08 15:21:34 +0200

    Update Client.js

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,3 +1,21 @@
-module.exports = class Client extends EventEmitter {
+const axios = require('axios');
 
+module.exports = class Client extends EventEmitter {
+	constructor() {
+		super()
+	}
+	
+	login(cookie) {
+		// Consistent endpoint for cookie verification
+		axios.get('https://groups.roblox.com/v1/groups/7/audit-log', {
+			headers: {
+				cookie: '.ROBLOXSECURITY=' + cookie + ';'
+			}
+		}).then(() => {
+			client.cookie = cookie;
+		}).catch(err => {
+			console.error(err);
+			console.error('Invalid cookie?');
+		});
+	}
 };
