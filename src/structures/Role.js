c44f05d6eb372c8f1de3e307fd4e27f96dfdd2c2
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2019-09-08 13:49:14 +0200

    Update Role.js

--- src/structures/Role.js
+++ src/structures/Role.js
@@ -4,6 +4,7 @@ module.exports = class Role extends Base {
 	constructor(client, data, group) {
 		super(client);
 		Object.defineProperty(this, 'group', { value: group });
+		group.addRole(this)
 		Object.defineProperty(this, 'id', { value: data.id });
 		this.name = data.name;
 		this.rank = data.rank;
