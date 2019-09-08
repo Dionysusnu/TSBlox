727b097a66e47ac7ce89115abd53a6105883fd22
Dionysusnu <dion.niessen@gmail.com>
2019-09-08 19:41:14 +0200

    add collection.first

--- src/structures/Collection.js
+++ src/structures/Collection.js
@@ -4,4 +4,8 @@ module.exports = class Collection extends Map {
 		Object.defineProperty(this, 'client', { value: client });
 		if (iterable) for (const item of iterable) this.set(item.id, item);
 	}
+
+	first() {
+		return this.values().next().value;
+	}
 };
