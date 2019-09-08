9dd81df06e4b027eab6d1996d6c5a0f5fd7486c2
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2019-09-08 12:40:34 +0200

    Update Group.fillData()

--- src/structures/Group.js
+++ src/structures/Group.js
@@ -2,14 +2,33 @@ const axios = require('axios');
 
 const Base = require('./Base');
 const Collection = require('./Collection');
+const GroupMember = require('./GroupMember');
 
 module.exports = class Group extends Base {
 	constructor(client) {
 		super(client);
 	}
 
-	fillData() {
-		const members = axios();
+	async fillData() {
+		const url = 'https://groups.roblox.com/v1/groups/' + this.id + '/users';
+		const initialResponse = await axios.get(url, { params: {
+			limit: 100
+		}});
+		let nextCursor = response.body.nextPageCursor
+		const responseMembers = [];
+		for (const user of initialResponse.body.data) {
+			responseMembers.push(new GroupMember(user));
+		}
+		while (nextCursor) {
+			const response = await axios.get(url, { params: {
+				cursor: nextCursor,
+				limit: 100
+			}});
+			nextCursor = response.body.nextPageCursor;
+			for (const user of response.body.data) {
+				responseMembers.push(new GroupMember(user));
+			}
+		}
 		this.members = new Collection(this.client, members);
 	}
 };
