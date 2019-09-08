c0e2e34db3a5e68c9148df7c9e89ca8075d24011
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2019-09-08 13:24:22 +0200

    Update GroupMember.js

--- src/structures/GroupMember.js
+++ src/structures/GroupMember.js
@@ -1,1 +1,15 @@
+const Base = require('./Base');
+const Role = require('./Role');
 
+module.exports = class GroupMember extends Base {
+	constructor(client, data, group) {
+		super(client);
+    Object.defineProperty(this, 'user', { value: new User(client, data.user) });
+    Object.defineProperty(this, 'group', { value: group });
+    this.role = group.roles.get(data.role.id) || new Role(client, data, group);
+	}
+  
+  setRole(role) {
+    // WIP
+  }
+};
