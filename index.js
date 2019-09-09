0a6dc9cd2e5a68e215120ebf74b2bd6ce379972d
Dionysusnu <dion.niessen@gmail.com>
2019-09-09 15:54:18 +0200

    add Shout export

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
 	Group: require('./src/structures/Group'),
 	GroupMember: require('./src/structures/GroupMember'),
 	Role: require('./src/structures/Role'),
+	Shout: require('./src/structures/User'),
 	User: require('./src/structures/User'),
 };
