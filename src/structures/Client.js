1e294d29c05dbe09cab8ce5b4d6d25c5c50ceb38
Dionysusnu <dion.niessen@gmail.com>
2019-09-09 17:44:55 +0200

    update docs for client

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -11,16 +11,29 @@ const Collection = require('./Collection');
 const Group = require('./Group');
 const Util = require('../Util');
 
+/** Client for interacting with the API */
 module.exports = class Client extends EventEmitter {
+	/**
+	 * Returns a new client
+	 * @param {string} [cookie] The .ROBLOSECURITY cookie to use when this client makes requests to the API
+	 */
 	constructor(cookie) {
 		super();
 		if (cookie) this.login(cookie);
+		/**
+		 * @property {Collection} groups A collection with all cached groups
+		 * @property {Collection} users A collection with all cached users
+		 */
 		this.groups = new Collection();
 		this.users = new Collection();
 		this.httpQueue = [];
 		this.util = Util;
 	}
 
+	/**
+	 * Logs in the client with the given cookie. Can be used after already setting a cookie
+	 * @param {string} cookie The cookie to login with
+	 */
 	login(cookie) {
 		this.cookie = cookie;
 		// Consistent endpoint for cookie verification, using roblox fan group which hopefully won't be deleted
@@ -57,6 +70,11 @@ module.exports = class Client extends EventEmitter {
 		}
 	}
 
+	/**
+	 * Internal function for making http calls to the API, taking care of cookie and X-CSRF tokens. Can be used for APIs that the library doesn't cover yet
+	 * @param {string} url The url to make an API call to
+	 * @param {Object} config The axios config
+	 */
 	http(url, config) {
 		return new Promise((resolve, reject) => {
 			if (!config) {
@@ -73,6 +91,10 @@ module.exports = class Client extends EventEmitter {
 		});
 	}
 
+	/**
+	 * Gets a group or updates its cached data
+	 * @param {IdResolvable} id The id of the group to get
+	 */
 	async getGroup(id) {
 		const response = await this.http('https://groups.roblox.com/v1/groups/' + id);
 		const cached = this.groups.get(id);
