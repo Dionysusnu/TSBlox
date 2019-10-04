12376a62693b327db16d8d69f4ac16c34be0d935
Dionysusnu <dion.niessen@gmail.com>
2019-10-04 15:00:32 +0200

    Add Client.debug = true to tests

--- tests/index.js
+++ tests/index.js
@@ -4,6 +4,7 @@ const { Client } = require('..');
 const client = new Client();
 
 async function doSomething() {
+	client.debug = true;
 	await require('./getGroup')(client, assert);
 	await require('./getRoles')(client, assert);
 	await require('./shout')(client, assert);
