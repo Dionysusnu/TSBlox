9f9960f8822b6c77bb53d88fe6e86a19d4cb655b
Dionysusnu <dion.niessen@gmail.com>
2019-10-07 14:41:13 +0200

    Add Collection utility methods Collection.firstKey Collection.last Collection.lastKey Collection.array Collection.keyArray

--- src/structures/Collection.ts
+++ src/structures/Collection.ts
@@ -13,6 +13,28 @@ export class Collection<stores> extends Map<number, stores> {
 		return this.values().next().value;
 	}
 
+	firstKey(): number {
+		return this.keys().next().value;
+	}
+
+	last(count: number): stores | Array<stores> {
+		const arr = this.array();
+		if (count === undefined) {
+			return arr[arr.length - 1];
+		} else {
+			return arr.slice(-count);
+		}
+	}
+
+	lastKey(count: number): number | Array<number> {
+		const arr = this.keyArray();
+		if (count === undefined) {
+			return arr[arr.length - 1];
+		} else {
+			return arr.slice(-count);
+		}
+	}
+
 	/**
 	 * Returns true if the object is in this collection
 	 * @param {Object} search The object to search for
@@ -35,5 +57,13 @@ export class Collection<stores> extends Map<number, stores> {
 			}
 		}
 		return filtered;
+	}
+
+	array(): Array<stores> {
+		return [...this.values()];
+	}
+
+	keyArray(): Array<number> {
+		return [...this.keys()];
 	}
 }
